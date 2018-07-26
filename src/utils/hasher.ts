397b0651ccc5fafbd2181604b1bbcf37aa26d560
zlumer <zlumer@gmail.com>
2018-07-26 20:02:40 +0300

    hash strings like objects

--- src/utils/hasher.ts
+++ src/utils/hasher.ts
@@ -2,8 +2,14 @@ import { IDataHashSource } from "../IOracleData";
 
 type Obj = {[key: string]: string | number | boolean }
 
-export let hashObj = (obj: Obj) => hash(prepare(obj))
-export let prepare = (obj: Obj) => Object.keys(obj).sort().map(key => `${key}=${obj[key]}`).join(";")
+export let hashObj = (obj: Obj | string) => hash(prepare(obj))
+export let prepare = (obj: Obj | string) =>
+{
+	if (typeof obj === "string")
+		return obj
+	
+	return Object.keys(obj).sort().map(key => `${key}=${obj[key]}`).join(";")
+}
 
 const createKeccakHash = require('keccak')
 export let keccak256 = () => createKeccakHash('keccak256')
