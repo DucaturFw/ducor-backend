b1a8d4210bfb64b4c63e28d34c7189ef5b3302a5
zlumer <zlumer@gmail.com>
2018-07-26 18:09:50 +0300

    api methods

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,7 +2,26 @@ import express from "express"
 
 export let app = express()
 
-app.get('/time', (req, res) =>
-{
-	res.json({ time: Date.now() })
-})
+export let CONFIG = {
+	config: () => ({ }),
+	generate: ({blockchain, category, provider, updatefreq, lifetime}: {
+		blockchain: string
+		category: string
+		provider: string
+		updatefreq: string
+		lifetime: string
+	}) => ({
+		contract: `generated_contract_${blockchain}_${category}_${provider}_${updatefreq}_${lifetime}`,
+		instructions: "contract_instructions"
+	})
+}
+
+app.get('/time', (req, res) => res.json({ time: Date.now() }))
+app.get('/config', (req, res) => res.json(CONFIG.config()))
+app.get('/generate/:blockchain/:category/:provider', (req, res) => res.json(CONFIG.generate({
+	blockchain: req.params.blockchain,
+	category: req.params.category,
+	provider: req.params.provider,
+	updatefreq: req.query.updatefreq,
+	lifetime: req.query.lifetime,
+})))
