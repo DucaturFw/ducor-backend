885f4b104392c455d77779be84968a38b8d378e8
Aler Denisov <aler.zampillo@gmail.com>
2018-07-30 19:13:59 +0300

    uint type

--- src/IOracleData.ts
+++ src/IOracleData.ts
@@ -1,41 +1,39 @@
-export type IDataType = "bytes" | "int" | "float" | "string" | "price"
-export interface IDataGeneric<TType extends IDataType, TData>
-{
-	type: TType
-	data: TData
+export type IDataType = "bytes" | "int" | "float" | "string" | "price" | "uint"
+export interface IDataGeneric<TType extends IDataType, TData> {
+  type: TType
+  data: TData
 }
 export type IOracleData =
-	| IDataGeneric<"bytes", string | Buffer>
-	| IDataGeneric<"int", number>
-	| IDataGeneric<"float", number>
-	| IDataGeneric<"string", string>
-	| IDataGeneric<"price", { price: number, decimals: number }>
+  | IDataGeneric<"bytes", string | Buffer>
+  | IDataGeneric<"int", number>
+  | IDataGeneric<"uint", number>
+  | IDataGeneric<"float", number>
+  | IDataGeneric<"string", string>
+  | IDataGeneric<"price", { price: number; decimals: number }>
 
 type FlatObj = { [key: string]: string | number | boolean }
-export interface IDataHashSource
-{
-	category: string
-	provider: string
-	ident: string
+export interface IDataHashSource {
+  category: string
+  provider: string
+  ident: string
 }
 
-export interface IDataDefinition extends IDataHashSource
-{
-	type: IDataType
-	hash: string
+export interface IDataDefinition extends IDataHashSource {
+  type: IDataType
+  hash: string
 }
 
-export interface IContractEndpoint
-{
-	hash: string
-	type: IDataType
-	name: string
+export interface IContractEndpoint {
+  hash: string
+  type: IDataType
+  name: string
 }
 
-export interface IContractEndpointSettings extends IContractEndpoint
-{
-	updateFreq: number
-	lifetime: number
+export interface IContractEndpointSettings extends IContractEndpoint {
+  updateFreq: number
+  lifetime: number
 }
 
-export type IContractGenerator = (endpoints: IContractEndpointSettings[]) => string
+export type IContractGenerator = (
+  endpoints: IContractEndpointSettings[]
+) => string
