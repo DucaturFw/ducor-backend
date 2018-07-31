7ef191aca6efd4401fca47cdbaaaf4610561788c
Aler Denisov <aler.zampillo@gmail.com>
2018-07-31 18:11:49 +0300

    custom process enviroment typings

---
+++ @types/node.d.ts
@@ -0,0 +1,12 @@
+declare namespace NodeJS {
+  export interface ProcessEnv {
+    DUCOR_EOS_WATCH_DELAY: string
+    DUCOR_EOS_CHAINID: string
+    DUCOR_EOS_ENDPOINT: string
+    DUCOR_EOS_MASTER_ORACLE: string
+    DUCOR_EOS_RETHINKHOST: string
+    DUCOR_EOS_RETHINKPORT: string
+    DUCOR_EOS_RETHINKDATABASE: string
+    DUCOR_EOS_RETHINKTABLE: string
+  }
+}
