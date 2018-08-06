978f3926fcdd81dad470df0a734c07607b0390fd
zlumer <zlumer@gmail.com>
2018-08-06 19:33:08 +0300

    acceptance tests on Bitfinex (#11) and ETH (#12)

--- src/api/index.test.ts
+++ src/api/index.test.ts
@@ -7,12 +7,17 @@ import "jest-extended"
 
 describe('api responses', () =>
 {
+	const URL = `http://localhost:40789`
+
 	let oldCfg = CONFIG.config
 	let oldGen = CONFIG.generate
 
 	let server: Server
-	beforeEach(done => server = app.listen(40789, done))
-	afterEach(done =>
+	beforeAll(done =>
+	{
+		server = app.listen(40789, done)
+	})
+	afterAll(done =>
 	{
 		CONFIG.config = oldCfg
 		CONFIG.generate = oldGen
@@ -21,16 +26,16 @@ describe('api responses', () =>
 	})
 	it('should ping', async () =>
 	{
-		let res = await axios(`http://localhost:40789/time`)
+		let res = await axios(`${URL}/time`)
 		expect(res).toBeDefined()
 		expect(res.data).toBeObject()
 		expect(res.data.time).toBeDefined()
 		expect(res.data.time).toBeNumber()
 		expect(res.data.time).toBeWithin(Date.now() - 1000, Date.now() + 1000)
 	})
-	it('should return empty config', async () =>
+	it('should return empty config by default', async () =>
 	{
-		let res = await axios(`http://localhost:40789/config`)
+		let res = await axios(`${URL}/config`)
 		expect(res).toBeDefined()
 		expect(res.data).toBeObject()
 		expect(res.data).toEqual({})
@@ -39,7 +44,7 @@ describe('api responses', () =>
 	{
 		CONFIG.config = () => ({ hello: "world" })
 
-		let res = await axios(`http://localhost:40789/config`)
+		let res = await axios(`${URL}/config`)
 		expect(res).toBeDefined()
 		expect(res.data).toBeObject()
 		expect(res.data).toEqual({ hello: "world" })
@@ -48,7 +53,7 @@ describe('api responses', () =>
 	{
 		CONFIG.config = config
 
-		let res = await axios(`http://localhost:40789/config`)
+		let res = await axios(`${URL}/config`)
 		
 		expect(res).toBeDefined()
 		expect(res.data).toBeObject()
@@ -65,5 +70,24 @@ describe('api responses', () =>
 		binance = binance!
 		expect(binance.types).toBeArray()
 		expect(binance.types).toContain("ETH/BTC")
+	})
+	;['binance', 'bitfinex'].forEach(exch =>
+	{
+		;['eos', 'eth'].forEach(blockchain =>
+		{
+			it(`should return ${exch} contract on ${blockchain}`, async () =>
+			{
+				CONFIG.config = config
+				CONFIG.generate = generate
+
+				let res = await axios(`${URL}/generate/${blockchain}/crypto/${exch}/ETH%2FBTC?updatefreq=100&lifetime=1000`)
+				expect(res.status).toEqual(200)
+				let data = res.data
+				expect(data).toContainAllKeys(['contract', 'instructions'])
+				expect(data.contract).toBeString()
+				expect(data.contract).not.toEqual('...')
+				expect(data.contract.length).toBeGreaterThan(100)
+			})
+		})
 	})
 })
