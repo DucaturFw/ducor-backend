ea3730c7ec3e145aa277c958677ed6269e025393
zlumer <zlumer@gmail.com>
2018-08-14 16:57:12 +0300

    random numbers data provider

--- src/providers/index.ts
+++ src/providers/index.ts
@@ -1,11 +1,18 @@
 import { request as fake } from "./fake"
 import { request as binance } from "./crypto/binance"
 import { request as bitfinex } from "./crypto/bitfinex"
+import { getArray as randomArray, getInteger as randomInt } from "./random/random"
 
 export let providers = {
-	fake,
-	binance,
-	bitfinex
+	crypto: {
+		fake,
+		binance,
+		bitfinex,
+	},
+	random: {
+		number: randomInt,
+		array: randomArray,
+	},
 }
 
 export { types } from "./types"
